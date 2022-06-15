ea35fcd1c8e08a89d075a32a881b271208033567
Eva Malluta <eva.malluta14@gmail.com>
2022-06-15 22:09:29 +0200

    CHANGE

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 let item=["Milk", "Cocoa", "Salad", "Carrots","Tomatoes", "Ready meals"]
 let price=[1.2, 2, 2, 2, 2.5, 5]
+let item1=[]
+let price1=[]
+item1.push(prompt("Add an item:"))
+price1.push(prompt("Add the price of the item:"))
+item.push(item1)
+price.push(price1)
 for (var i=0; i<item.length; i++){
 var namestring = item[i]+ ": " + " £"+price[i].toFixed(2)
 console.log(namestring )
 }
+
+// item.push= prompt("Add an item?")
+// price=prompt("Add the price?")
+// console.log(item,price)
