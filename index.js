20fb1c735a931de762b3f3429bef083d66808403
Eva Malluta <eva.malluta14@gmail.com>
2022-06-15 20:28:40 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let item=["Milk", "Cocoa", "Salad", "Carrots","Tomatoes", "Ready meals"]
+let price=[1.2, 2, 2, 2, 2.5, 5]
+for (var i=0; i<item.length; i++){
+var namestring = item[i]+ ":" + price[i]
+console.log(namestring)
+}
