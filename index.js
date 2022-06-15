75ef7541c65b2e9b65e46eea891bdafa8882e08c
Eva Malluta <eva.malluta14@gmail.com>
2022-06-15 20:50:00 +0200

    change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let item=["Milk", "Cocoa", "Salad", "Carrots","Tomatoes", "Ready meals"]
 let price=[1.2, 2, 2, 2, 2.5, 5]
 for (var i=0; i<item.length; i++){
-var namestring = item[i]+ ":" + price[i]
-console.log(namestring)
+var namestring = item[i]+ ": " + " £"+price[i].toFixed(2)
+console.log(namestring )
 }
